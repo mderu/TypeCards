996c006ec051f7405a20a2985b8e55877c9589ca
Johnson Johnsson <JohnsonJohnsson@users.noreply.github.com>
2018-10-06 22:17:17 -0700

    npm run start keeps deleting public/build

--- webpack.build.js
+++ webpack.build.js
@@ -2,6 +2,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devtool: 'inline-source-map',
   entry: './src/app.tsx',
   plugins: [
     new CleanWebpackPlugin(['public/build']),
